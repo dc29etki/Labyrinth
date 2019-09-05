73f2b5a41109b7e09a3feb684294710738c25084
Jacob Rolnick <rolnicj@sunypoly.edu>
2019-09-05 10:24:39 -0400

    Changed screen to display card png and teal color (instead of defaults)

--- libGDX Practice Projects/core/src/com/mygdx/game/Labyrinth.java
+++ libGDX Practice Projects/core/src/com/mygdx/game/Labyrinth.java
@@ -13,12 +13,12 @@ public class Labyrinth extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("Card_Base.png");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 1, 5, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
