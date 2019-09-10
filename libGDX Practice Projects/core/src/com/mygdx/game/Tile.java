58ab9fe76209a542b3d09819d93302c19f266ed4
Jacob Rolnick <rolnicj@sunypoly.edu>
2019-09-10 10:29:31 -0400

    Created BASIC tile class

---
+++ libGDX Practice Projects/core/src/com/mygdx/game/Tile.java
@@ -0,0 +1,26 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
+
+
+public class Tile {
+
+    private Texture tilePng;
+    private Texture treasurePng;
+    private Rectangle locationRect;
+
+    public Tile(int tileDir, int treasureNum, int xPos, int yPos){
+    //tileDir: 0 = Corner, 1 = Straight, 2 = Intersection; treasureNum = treasure (0 = none); xPos = column number (in board); yPos = row number (in board)
+
+
+    }
+
+
+}
