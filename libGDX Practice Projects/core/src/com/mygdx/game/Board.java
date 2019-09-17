6cfe2c1ece18ed3d2d067e75cc2f7fc37574df84
akeemclarke <54855975+akeemclarke@users.noreply.github.com>
2019-09-17 11:11:05 -0400

    Updated

--- libGDX Practice Projects/core/src/com/mygdx/game/Board.java
+++ libGDX Practice Projects/core/src/com/mygdx/game/Board.java
@@ -6,7 +6,7 @@ import java.util.Random;
 public class Board {
     private Tile[][] grid;
     public Board(){
-        grid = createGrid(7,7);
+        grid = createGrid(12,12);
     }
     public Board(int x, int y){
         grid = createGrid(x,y);
@@ -14,12 +14,13 @@ public class Board {
     public Tile[][] createGrid(int x, int y){
         Tile[][] grid = new Tile[x][y];
         Random rand = new Random();
-        for(int i=0; i<x; i++){
-            for(int j=0; j<y; i++){
+        for(int i=0; i<10; i++){
+            for(int j=i; j<10; i++){
                 int tileDir = rand.nextInt(2);
                 int xPos = rand.nextInt(x);
                 int yPos = rand.nextInt(y);
-                grid[i][j] = new Tile(tileDir,0,xPos,yPos,0);
+                grid[j][i] = new Tile(tileDir,0,xPos,yPos,0);
+                System.out.println(i+"  "+j);
             }
         }
         return grid;
