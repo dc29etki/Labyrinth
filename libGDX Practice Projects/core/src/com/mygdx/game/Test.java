850acb0a734bd620e8eb57bbab3829aff4a57736
akeemclarke <54855975+akeemclarke@users.noreply.github.com>
2019-10-03 08:56:28 -0400

    SplashScreen/MainMenu

--- libGDX Practice Projects/core/src/com/mygdx/game/Test.java
+++ libGDX Practice Projects/core/src/com/mygdx/game/Test.java
@@ -10,7 +10,7 @@ public class Test extends Game{
     }
     @Override
     public void create() {
-        setScreen(new SplashScreen());
+
     }
 
 
--- libGDX Practice Projects/core/src/com/mygdx/game/camera/Camera.java
+++
@@ -1,9 +0,0 @@
-package com.mygdx.game;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-public class Cam extends OrthographicCamera{
-    public Cam(float width, float height){
-        super((float)width, (float)height);
-        this.position.x = (float)(width/2);
-        this.position.y = (float)(width/2);
-    }
-}
