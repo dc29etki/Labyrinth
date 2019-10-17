67d535141b672b45b300b3c16e66aef5c8c6df49
Jacob Rolnick <rolnicj@sunypoly.edu>
2019-10-17 11:52:50 -0400

    Create Player.java

---
+++ libGDX Practice Projects/core/src/com/mygdx/game/Player.java
@@ -0,0 +1,44 @@
+package com.mygdx.game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+public class Player {
+    int[] boardPosition;
+    Texture smallTexture;
+    Texture bigTexture;
+    Sprite playerSprite;
+    int color;
+    Card[] hand;
+    int[] drawPos;
+    boolean myTurn;
+
+    public Player(int playerNum){
+        switch(playerNum){
+            case -3:
+                smallTexture = new Texture(Gdx.files.internal("Icon_BlueCircle.png"));
+                setBoardPosition(0,6);
+                break;
+            case -4:
+                smallTexture = new Texture(Gdx.files.internal("Icon_YellowCircle.png"));
+                setBoardPosition(6,0);
+                break;
+            case -5:
+                smallTexture = new Texture(Gdx.files.internal("Icon_GreenCircle.png"));
+                setBoardPosition(6,6);
+                break;
+            case -2:
+            default:
+                smallTexture = new Texture(Gdx.files.internal("Icon_RedCircle.png"));
+                setBoardPosition(0,0);
+                break;
+        }
+    }
+
+    void setBoardPosition(int x, int y){
+
+        //WIP
+
+    }
+
+}
