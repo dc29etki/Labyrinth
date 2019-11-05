7b826cebffd954de7432c582bcd31ec15b0dfb51
Jacob Rolnick <rolnicj@sunypoly.edu>
2019-11-05 11:46:51 -0500

    Revert "Merge branch 'develop2' into Game-Runner"
    This reverts commit 6b082ab7863242e358548497097b7fe6a586f33b, reversing
    changes made to ae7dfef79b5d89025273150f252ec03cd238f31b.

--- libGDX Practice Projects/core/src/com/mygdx/game/Deck.java
+++ libGDX Practice Projects/core/src/com/mygdx/game/Deck.java
@@ -11,8 +11,7 @@ public class Deck {
     private int dealType;
 
     public Deck(int deckType, int treasuresType){
-        //DeckType: 0 = cards, 1 = corner set tiles,
-        // 2 = odds set tiles; TreasuresType: determines treasure type.
+        //DeckType: 0 = cards, 1 = corner set tiles, 2 = odds set tiles; TreasuresType: determines treasure type.
 
         treasureListLength = Treasures.getTreasureDict(treasuresType).length;
 
--- libGDX Practice Projects/core/src/com/mygdx/game/Tile.java
+++ libGDX Practice Projects/core/src/com/mygdx/game/Tile.java
@@ -1,10 +1,9 @@
 package com.mygdx.game;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import java.awt.*;
+
 import static com.mygdx.game.Labyrinth.Height;
 import static com.mygdx.game.Labyrinth.Width;
 
@@ -12,6 +11,8 @@ import static com.mygdx.game.Labyrinth.Width;
 public class Tile {
     private int[] connections;
     private Texture tilePng;
+    private Texture tileMovePng;
+    private boolean isShowingMove = false;
     private int treasureId;
     private int[] arrayPos;
     private Texture treasurePng;
@@ -22,41 +23,44 @@ public class Tile {
 
     public Tile(int tileType, int treasureNum, int xPos, int yPos, int dir, int treasuresType){
         //Connections: Top,Right,Bottom,Left
+        Texture[] tilePngs = Treasures.getTileSprites(treasuresType);
         if(tileType == 0){//Corner Piece
             connections = new int[]{0,1,1,0};
-            tilePng = new Texture(Gdx.files.internal("Tile_23.png"));
+            tilePng = tilePngs[0];
+            tileMovePng = tilePngs[3];
         }else if(tileType == 1){//Straight Piece
             connections = new int[]{0,1,0,1};
-            tilePng = new Texture(Gdx.files.internal("Tile_13.png"));
+            tilePng = tilePngs[1];
+            tileMovePng = tilePngs[4];
         }else {//T-Piece
             connections = new int[]{1,1,0,1};
-            tilePng = new Texture(Gdx.files.internal("Tile_234.png"));
+            tilePng = tilePngs[2];
+            tileMovePng = tilePngs[5];
         }
 
         arrayPos = new int[]{xPos,yPos};
 
         treasureId = treasureNum;
-        String[] treasureDict = Treasures.getTreasureDict(treasuresType);
 
         findPlaceLocation();
         findTreasureLocation();
 
         if(treasureId > -1){
-            treasurePng = new Texture(Gdx.files.internal("Icon_" + treasureDict[treasureId]+".png"));
+            treasurePng = Treasures.getTreasure(treasureId,treasuresType);
         }else if(treasureId < -1){
             if(treasureNum == -2){//Red Circle
-                treasurePng = new Texture(Gdx.files.internal("Icon_" + "RedCircle" + ".png"));
+                treasurePng = Treasures.getPlayerTextures(treasuresType)[0];
             }else if(treasureNum == -3){//Blue Circle
-                treasurePng = new Texture(Gdx.files.internal("Icon_" + "BlueCircle" + ".png"));
+                treasurePng = Treasures.getPlayerTextures(treasuresType)[1];
             }else if(treasureNum == -4){//Yellow Circle
-                treasurePng = new Texture(Gdx.files.internal("Icon_" + "YellowCircle" + ".png"));
+                treasurePng = Treasures.getPlayerTextures(treasuresType)[2];
             }else if(treasureNum == -5) {//Green Circle
-                treasurePng = new Texture(Gdx.files.internal("Icon_" + "GreenCircle" + ".png"));
+                treasurePng = Treasures.getPlayerTextures(treasuresType)[3];
             }else{
-                treasurePng = new Texture(Gdx.files.internal("Blank_Icon.png"));
+                treasurePng = Treasures.getBlankTexture();
             }
         }else{
-            treasurePng = new Texture(Gdx.files.internal("Blank_Icon.png"));
+            treasurePng = Treasures.getBlankTexture();
         }
 
         thisTile = new Sprite(tilePng);
@@ -100,6 +104,7 @@ public class Tile {
         for(int i = 0; i < arrayPos[1]; i++){
             y -= Height/10 + 1;
         }
+
         if(arrayPos[0] == -1){
             x = 9*(Width/10 + 1);
             y = Height - 9*(Height/10 + 1);
@@ -143,6 +148,15 @@ public class Tile {
     int[] getTilePosition(){
         return tilePos;
     }
+
+    void showLine() {
+        if(isShowingMove){
+            thisTile.setTexture(tilePng);
+        }else{
+            thisTile.setTexture(tileMovePng);
+        }
+    }
+
     @Override
     public String toString(){
         return "Treasure: " + treasureId + ", Board Position: (" + arrayPos[0] + "," + arrayPos[1] + "), Tile Connected Sides: " + connections[0] + connections[1] + connections[2] + connections[3];
